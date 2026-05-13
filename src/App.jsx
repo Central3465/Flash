26a9666de3de86786c917a9595d94001cae67144
Hanlin Bai <hanlinbai@hotmail.com>
2026-05-13 20:46:14 +0100

    Updated gh-pages for deployment.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, BrowserRouter } from 'react-router-dom'
 import Header from './components/Header'
 import HomePage from './pages/HomePage'
 import AboutPage from './pages/AboutPage'
@@ -6,13 +6,16 @@ import AboutPage from './pages/AboutPage'
 function App() {
   return (
     <div className="app">
-      <Header />
-      <main>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/about" element={<AboutPage />} />
-        </Routes>
-      </main>
+      {/* Notice the brackets are removed from the basename */}
+      <BrowserRouter basename="/Flash">
+        <Header />
+        <main>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/about" element={<AboutPage />} />
+          </Routes>
+        </main>
+      </BrowserRouter>
     </div>
   )
 }
